736cacb3c3ed74c82efb1b99097060b893e059c1
Jacuancum <cks2306@gmail.com>
2024-02-18 16:39:38 +0800

    responsive temple image added

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -3,4 +3,4 @@ const hambutton = document.querySelector('#menu');
 hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('show');
     hambutton.classList.toggle('show');
-})
+});
