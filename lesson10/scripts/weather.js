faa747d5d9d40e2d5c2372c968792fe17ed43a70
Jacob Chan <cks2306@gmail.com>
2024-03-17 07:16:44 +0800

    lesson10 weather

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,30 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat={22.41526}&lon={114.08942}&appid={b819e64251633b78d3925158881882f3}';
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data._____}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${______}.___`;
+    let desc = data.weather[0].______;
+    weatherIcon.setAttribute('___', _____);
+    weatherIcon.setAttribute('___', _____);
+    captionDesc.textContent = `${desc}`;
+  }
+
+  async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // testing only
+        // displayResults(data); // uncomment when ready
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
