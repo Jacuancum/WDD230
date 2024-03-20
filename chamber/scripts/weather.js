7c4cdda1294d3f2bdfca16efa8f5f1b10ecb1165
Jacuancum <cks2306@gmail.com>
2024-03-20 14:57:36 +0800

    href update

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -21,21 +21,20 @@ const url = 'https://api.openweathermap.org/data/2.5/weather?lat=22.41526&lon=11
 
   function displayResults(data) {
     const forecasts = data.list.slice(0,3);
-    forecasts.foreach((forecast, index)=> {
+    forecasts.forEach((forecast, index)=> {
       const Celsiustemp = Math.round(forecast.main.temp - 273.15);
-      const date =new Date(forecasts.dt * 1000);
+      const date =new Date(forecast.dt * 1000);
       const day = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'][date.getDay()];
+
       const tempElement = document.createElement('p');
+      tempElement.innerHTML = `${day}: ${Celsiustemp}&deg;C`;
 
-      
-      currentTemp.innerHTML = `${day}: ${Celsiustemp}&deg;C`;
-      const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-      let desc = data.weather[0].description;
+      document.querySelector('.topcard').appendChild(tempElement);
+
+      const iconsrc = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
       weatherIcon.setAttribute('src', iconsrc);
-      weatherIcon.setAttribute('alt', desc);
-      captionDesc.textContent = `${desc}`;
-
-      document.body.appendChild(tempElement);
+      weatherIcon.setAttribute('alt', forecast.weather[0].description);
+        captionDesc.textContent = forecast.weather[0].description;
     });
   }
 
