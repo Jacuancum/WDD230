04fd4b04daf9e876c025f8fd8be74c0658cdb170
Jacuancum <cks2306@gmail.com>
2024-03-21 15:29:35 +0800

    footer update

--- chamber/scripts/last modification.js
+++ chamber/scripts/last modification.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const lastModified = new Date(document.lastModified);
-    const lastModifiedElement = document.getElementById('last-modified');
+    const lastModifiedElement = document.getElementById('last_modified');
     lastModifiedElement.textContent = lastModified.toLocaleString();
   });
