6ec394a56257ad80f814526c9142dc739f5e318c
Jacuancum <cks2306@gmail.com>
2024-03-21 13:55:36 +0800

    chamber logo

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,3 +1,4 @@
+/*
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
@@ -38,6 +39,7 @@ function displayResults(data) {
   weatherIcon.setAttribute('alt', forecasts[0].weather[0].description);
   captionDesc.textContent = forecasts[0].weather[0].description;
 }
+*/
 
 document.addEventListener('DOMContentLoaded', function() {
   const today = new Date();
@@ -53,3 +55,34 @@ document.addEventListener('DOMContentLoaded', function() {
       });
   }
 });
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=22.41526&lon=114.08942&appid=b819e64251633b78d3925158881882f3';
+
+  async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // testing only
+        displayResults(data); // uncomment when ready
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  apiFetch();
+
+  function displayResults(data) {
+    const Celsiustemp = Math.round(data.main.temp - 273.15);
+    currentTemp.innerHTML = `${Celsiustemp}&deg;C`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+  }
