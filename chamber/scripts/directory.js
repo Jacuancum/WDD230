91010a13eb05de329356b83d9e00d4ef63d9956a
Jacuancum <cks2306@gmail.com>
2024-03-22 12:02:21 +0800

    directory grid and list fixed

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -3,7 +3,7 @@ const listbutton = document.querySelector("#list");
 const display = document.querySelector("article");
 
 // The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
-
+/*
 gridbutton.addEventListener("click", () => {
 	display.classList.add("grid");
 	display.classList.remove("list");
@@ -14,4 +14,13 @@ listbutton.addEventListener("click", showList);
 function showList() {
 	display.classList.add("list");
 	display.classList.remove("grid");
+}
+*/
+
+gridbutton.addEventListener("click", () => toggleView("grid"));
+listbutton.addEventListener("click", () => toggleView("list"));
+
+function toggleView(view) {
+    display.classList.toggle("grid", view === "grid");
+    display.classList.toggle("list", view === "list");
 }
