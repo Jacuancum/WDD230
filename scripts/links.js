f908a750ab93e0d1163c23657d1f50b5afbdf8bb
Jacuancum <cks2306@gmail.com>
2024-04-02 09:49:14 +0800

    lesson09 assignment re-do

---
+++ scripts/links.js
@@ -0,0 +1,37 @@
+const baseURL = "https://jacuancum.github.io/wdd230/";
+const linksURL = "https://jacuancum.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    /*console.log(data);*/
+    displayLinks(data.lessons);
+  }
+  
+  getLinks();
+
+  const displayLinks = (lessons) => {
+    const lessonList = document.createElement('ul');
+
+    lessons.forEach((lesson) => {
+        const lessonItem = document.createElement('li');
+        const lessonNumber = document.createElement('span');
+        lessonNumber.textContent = `Lesson ${lesson.lesson}`;
+        lessonItem.appendChild(lessonNumber);
+
+        const linksList = document.createElement('ul');
+        lesson.links.forEach((link) => {
+            const linkItem = document.createElement('li');
+            const linkElement = document.createElement('a');
+            linkElement.href = link.url;
+            linkElement.textContent = link.title;
+            linkItem.appendChild(linkElement);
+            linksList.appendChild(linkItem);
+        });
+
+        lessonItem.appendChild(linksList);
+        lessonList.appendChild(lessonItem);
+    });
+
+    document.body.appendChild(lessonList);
+}
