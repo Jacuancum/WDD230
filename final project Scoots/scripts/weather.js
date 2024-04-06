d7c176b5906c533aa36dc2b8edff05f9ba7741dd
Jacuancum <cks2306@gmail.com>
2024-04-06 08:54:17 +0800

    fp update

---
+++ final project Scoots/scripts/weather.js
@@ -0,0 +1,29 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=22.41526&lon=114.08942&appid=b819e64251633b78d3925158881882f3';
+
+  async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  apiFetch();
+
+  function displayResults(data) {
+    const Celsiustemp = Math.round(data.main.temp - 273.15);
+    currentTemp.innerHTML = `${Celsiustemp}&deg;C`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+  }
