f859b57461b5ba24f70646f29ba9471a2e735210
Jacuancum <cks2306@gmail.com>
2024-04-08 17:11:44 +0800

    rental json update

--- final project Scoots/scripts/rentals.js
+++ final project Scoots/scripts/rentals.js
@@ -14,19 +14,23 @@ const displayRentals = (rentals) => {
         let fullName = document.createElement('h2');
         let portrait = document.createElement('img');
         let maxPersonsInfo = document.createElement('p');
+        let resHD = document.createElement('p');
+        let resFD = document.createElement('p');
+        let walHD = document.createElement('p');
+        let walFD = document.createElement('p');
 
         fullName.textContent = rental.name;
         portrait.setAttribute('src', rental.image);
         portrait.setAttribute('alt', `Portrait of ${rental.name}`);
         portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '500');
-        portrait.setAttribute('height', '360');
-        
+        portrait.setAttribute('width', '333');      
         maxPersonsInfo.textContent = `Max person: ${rental.maxPersons}`;
+        resHD.textContent = `Reservation Half Day (3hrs): ${rental.reservationHalfDay}`;
 
         card.appendChild(fullName);
         card.appendChild(portrait);
         card.appendChild(maxPersonsInfo);
+        card.appendChild(resHD);
         cards.appendChild(card);
     });
   };
